ff644f68620207983fac913cb04c5176cec03f5d
jverma205 <jatin.verma.205@gmail.com>
2022-05-10 16:56:26 +0800

    add minimum setup to run webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
 const path = require('path')
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "dist")
-    }
+        path: path.resolve(__dirname, "dist"),
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin( {
+            template: path.resolve(__dirname, './src/template.html')
+        } )
+    ],
+    devtool: 'cheap-source-map'
 }
